8d52b80b2554791a3d489fbdc1e37c1a1bfbd7ac
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2021-08-07 15:20:53 +0200

    Correctly support nullable (#4)

---
+++ examples/ConsoleAppConsumerNET472/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AnyOfTypes;
+
+namespace ConsoleAppConsumerNET472
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReturnSomething().CurrentValue);
+            Console.WriteLine(ReturnSomething().CurrentValue);
+            Console.WriteLine(ReturnSomething().CurrentValue);
+        }
+
+        private static AnyOf<string, int, bool> ReturnSomething()
+        {
+            switch (new Random().Next(3))
+            {
+                case 1:
+                    return "test";
+
+                case 2:
+                    return 42;
+
+                default:
+                    return true;
+            }
+        }
+    }
+}
