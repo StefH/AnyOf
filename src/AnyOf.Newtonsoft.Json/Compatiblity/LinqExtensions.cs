245dc13cb1546404895329dbe4e947b31db8699e
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-09-15 13:20:06 +0000

    AnyOfJsonConverter

---
+++ src/AnyOf.Newtonsoft.Json/Compatiblity/LinqExtensions.cs
@@ -0,0 +1,14 @@
+﻿#if !NETSTANDARD2_1_OR_GREATER
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    internal static class LinqExtensions
+    {
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source, IEqualityComparer<T> comparer = null)
+        {
+            return new HashSet<T>(source, comparer);
+        }
+    }
+}
+#endif
