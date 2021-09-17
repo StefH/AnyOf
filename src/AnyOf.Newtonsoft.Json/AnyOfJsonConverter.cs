09cc2127cdd402415eb0bb5eb94be5f51af5c1f2
Stef Heyenrath <stef.heyenrath@gmail.com>
2021-09-17 08:16:59 +0000

    .

--- src/AnyOf.Newtonsoft.Json/AnyOfJsonConverter.cs
+++ src/AnyOf.Newtonsoft.Json/AnyOfJsonConverter.cs
@@ -6,7 +6,7 @@ using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace AnyOfTypes
+namespace AnyOfTypes.Newtonsoft.Json
 {
     public class AnyOfJsonConverter : JsonConverter
     {
